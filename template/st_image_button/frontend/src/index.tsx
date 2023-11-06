9d5a809658ac465cb7988a112fa80763cd8a29b7
Anthony Pecquenard <anthony.pecquenard@ens-lyon.fr>
2023-11-06 15:31:21 +0100

    Update

---
+++ template/st_image_button/frontend/src/index.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import ImageButton from "./ImageButton"
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ImageButton />
+  </React.StrictMode>,
+  document.getElementById("root")
+)
